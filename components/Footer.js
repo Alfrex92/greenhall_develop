b065cf7d6011cea8a73cd4a7badf6cb676d50bb0
Alfrex <alfrex.r@gmail.com>
2018-07-22 17:24:37 +0900

    unused style tags were removed

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,5 @@
 export default () => (
     <footer>
         <p>hello</p>
-    <style jsx global> {`
-        footer {
-            background-color: #373737;
-        }
-    `}</style>
     </footer>
 )
--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -5,7 +5,5 @@ export default () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
     </Head>
-    <style jsx global>{
-    ``}</style>
   </div>
 )
