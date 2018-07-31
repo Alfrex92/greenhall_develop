fbc5585d6331c1036ef7ec567219394e9c1bd5f8
Alfrex <alfrex.r@gmail.com>
2018-08-01 01:17:22 +0900

    access map was commented out

--- pages/access.js
+++ pages/access.js
@@ -1,21 +1,21 @@
-import AccessMap from '../components/AccessMap'
-import Footer from '../components/Footer'
-import Home from '../components/Home'
-import Access from '../components/Access'
-import Head from 'next/head'
-import GlobalStyles from '../components/GlobalStyles'
-
-
-
-// add responsive problem
-export default () => (
-    <Home>
-        <GlobalStyles/>
-        <Head>
-            <meta name="description" content="testing the description of this " />
-        </Head>
-        <AccessMap/>
-        <Footer/>
-
-    </Home>
-)
+// import AccessMap from '../components/AccessMap'
+// import Footer from '../components/Footer'
+// import Home from '../components/Home'
+// import Access from '../components/Access'
+// import Head from 'next/head'
+// import GlobalStyles from '../components/GlobalStyles'
+//
+//
+//
+// // add responsive problem
+// export default () => (
+//     <Home>
+//         <GlobalStyles/>
+//         <Head>
+//             <meta name="description" content="testing the description of this " />
+//         </Head>
+//         <AccessMap/>
+//         <Footer/>
+//
+//     </Home>
+// )
