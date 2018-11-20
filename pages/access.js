a681338b605e24beb4e9541fb2c9aaf35de2efb4
Alfrex <alfrex.r@gmail.com>
2018-11-21 08:41:36 +0900

    replace email & map was changed

--- pages/access.js
+++ pages/access.js
@@ -23,12 +23,7 @@ export default () => (
         </Head>
         <div className="AccessMap">
             <div className="Cover ComponentSeparation AccessMap-content">
-                <div className="AccessMap-illustration">
-                    <img src="/static/img/green-hall_map.jpg" className="AccessMap-img"></img>
-                    <div className="Access-container">
-                        <a target="_blank" className="Access-link" href="https://www.google.co.jp/maps?q=%E2%80%8B%E3%80%92152-0035+%E6%9D%B1%E4%BA%AC%E9%83%BD+%E7%9B%AE%E9%BB%92%E5%8C%BA+%E8%87%AA%E7%94%B1%E3%81%8C%E4%B8%98+2-15-10+A%26D%E3%83%8F%E3%82%A6%E3%82%B9&um=1&ie=UTF-8&sa=X&ved=0ahUKEwis9rrAxo_cAhVQUd4KHSNDCxIQ_AUICigB">​〒152-0035 東京都 目黒区 自由が丘 2-15-10 A&Dハウス</a>
-                    </div>
-                </div>
+                
                 <div className="Cover-container  AccessMap-description">
                     <div className="Cover-description ">
                     <h1 className="t3 Cover-header">自由が丘グリーンホール　アクセス</h1>
@@ -42,7 +37,7 @@ export default () => (
                     </div>
                 </div>
             </div>
-
+            <Access/>    
         </div>
         <Footer/>
 
